fca1c58316b1e2f4fedd7b37de4e27c6c6e1c2a4
Jinaeres <ziadrassmi@gmail.com>
2023-03-16 09:12:48 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
-/**
- *  * main - Prints a string to the console
- *   *
- *    * Return: Always 0
- *    */
-
+/*
+ * main - Prints a string to the console
+ * Return: Always 0
+ */
 int main(void)
 {
 	        puts("\"Programming is like building a multilingual puzzle");
