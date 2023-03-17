066727bc73b36ba74f1f83d17b0a9c38a8d39c62
Jinaeres <ziadrassmi@gmail.com>
2023-03-17 12:29:30 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,15 +10,15 @@ int main(void)
 	char uppercase = 'A';
 
 	while (lowercase <= 'z')
-       	{
+	{
 		putchar(lowercase);
 		lowercase++;
 	}
 	while (uppercase <= 'Z')
-       	{
+	{
 		putchar(uppercase);
 		uppercase++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
