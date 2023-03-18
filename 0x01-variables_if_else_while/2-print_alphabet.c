0862a64f6380d9d191e308f4374bc676a4b93032
Jinaeres <ziadrassmi@gmail.com>
2023-03-18 08:39:26 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char lowercase = 'a';
+
+  while (lowercase <= 'z')
+    {
+      putchar(lowercase);
+      lowercase++;
+    }
+  putchar('\n');
+  return (0);
+}
