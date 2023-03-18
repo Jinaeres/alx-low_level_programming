c195312a3a9b5d521a0eea0f0a60fb8935e094d0
Jinaeres <ziadrassmi@gmail.com>
2023-03-18 08:41:11 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-  char lowercase = 'a';
+	char lowercase = 'a';
 
-  while (lowercase <= 'z')
-    {
-      putchar(lowercase);
-      lowercase++;
-    }
-  putchar('\n');
-  return (0);
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	putchar('\n');
+	return (0);
 }
