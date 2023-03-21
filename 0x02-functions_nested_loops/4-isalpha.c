1af3f01d01ab0a13ac8a8f2279b80e72274f0e28
Jinaeres <ziadrassmi@gmail.com>
2023-03-21 10:39:02 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_isalpha- Entry point
+ *@c: the character to check
+ *Return: Always int 1 if alpha  or 0 otherwise (Success)
+ */
+int _isalpha(int c)
+{
+	if ((int) c >= 97 && (int) c <= 122)
+		return (1);
+	else if ( c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
