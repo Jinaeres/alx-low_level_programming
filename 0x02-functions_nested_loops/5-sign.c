0bfe769cdb5769a6d68df707af1e693ce3f84399
Jinaeres <ziadrassmi@gmail.com>
2023-03-21 10:59:52 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,8 @@
 /**
  *print_sign - prints the sign of a number
  *@n: the number to check
- *Return: Always int 1 if positive  or -1 if negative or 0 if its 0 if its 0 (Success)
+ *Return: Always int 1 if positive  or -1 if negative
+ *Return: 0 if its 0 (Success)
  */
 int print_sign(int n)
 {
