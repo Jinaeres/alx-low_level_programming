466b3c07c136d78273f63f01fab37246aa86f149
Jinaeres <ziadrassmi@gmail.com>
2023-03-21 14:44:19 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- *print_last_digit - prints and return the last degit 
- *@c: the number to check
- *Return: Always int c if +  or -c otherwise (Success)
+ *jack_bauer-prints time
+ *
+ *Return: void (Success)
  */
 void jack_bauer(void)
 {
 	int hrs = 0;
 	int mins = 0;
 
-	while(hrs < 24)
+	while (hrs < 24)
 	{
 		mins = 0;
-		while(mins < 60)
+		while (mins < 60)
 		{
 			_putchar((hrs / 10) + '0');
 			_putchar((hrs % 10) + '0');
