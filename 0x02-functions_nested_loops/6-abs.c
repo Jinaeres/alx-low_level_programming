7c750d2ca48ed736bd394f2466746fa7376ba102
Jinaeres <ziadrassmi@gmail.com>
2023-03-21 13:41:17 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_abs  Entry point
+ *@c: the number to check
+ *Return: Always int c if +  or -c otherwise (Success)
+ */
+int _abs(int c)
+{
+	if (c > 0)
+		return (c);
+	else
+		return (-c);
+}
